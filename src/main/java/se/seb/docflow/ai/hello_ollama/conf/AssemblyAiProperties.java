d58f4d8908d8491fad175e040bb8c69f9abcd9f4
Bahram Jahanshahi <bahram.jahanshahi@gmail.com>
2024-10-11 13:57:42 +0200

    Remove lombok

--- src/main/java/se/seb/docflow/ai/hello_ollama/conf/AssemblyAiProperties.java
+++ src/main/java/se/seb/docflow/ai/hello_ollama/conf/AssemblyAiProperties.java
@@ -2,14 +2,16 @@ package se.seb.docflow.ai.hello_ollama.conf;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import lombok.Getter;
-import lombok.Setter;
+
 
 @Configuration
 @ConfigurationProperties(prefix = "assemblyai")
-@Getter
-@Setter
+
 public class AssemblyAiProperties {
 
     private String apiKey;
+
+    public String getApiKey() {
+        return apiKey;
+    }
 }
